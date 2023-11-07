c81fdabe62381f77b343e9862a82cad9b1094727
yomayo666 <yomayo1992@yandex.ru>
2023-11-07 19:32:33 +0300

    add: forked a new way of routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Lobarouter</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-      <main>  
-        <Routes>
-          <Route index element={<Home />}/>
-          <Route path="about" element={<About />}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <RouterProvider router={router}></RouterProvider>
+
   );
 }
 
---
+++ src/layouts/RootLayout.js
@@ -0,0 +1,18 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>Lobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
+  );
+}
