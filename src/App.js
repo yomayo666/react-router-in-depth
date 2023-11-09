138b40df0bc00781f9301dff19b597f36c1494cb
yomayo666 <yomayo1992@yandex.ru>
2023-11-09 17:29:42 +0300

    add: added detailed information abaut carears

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import {
-  createBrowserRouter, 
+  createBrowserRouter,
   createRoutesFromElements,
-  Route, 
-  RouterProvider
-} from 'react-router-dom'
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Faq from './pages/help/Faq'
-import Contact from './pages/help/Contact'
-import NotFound from './pages/NotFound'
-import Careers, { careersLoader } from './pages/careers/Careers'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Faq from "./pages/help/Faq";
+import Contact from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/careers/Careers";
 
 // layouts
-import RootLayout from './layouts/RootLayout'
-import HelpLayout from './layouts/HelpLayout'
-import CareersLayout from './layouts/CareersLayout'
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+import CareersLayout from "./layouts/CareersLayout";
+import CareersDetails, { careerDetailsLoader } from "./pages/careers/CareersDetails";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,25 +26,20 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact/>} />
+        <Route path="contact" element={<Contact />} />
       </Route>
       <Route path="careers" element={<CareersLayout />}>
-        <Route 
-          index 
-          element={<Careers />} 
-          loader={careersLoader} 
-        />
+        <Route index element={<Careers />} loader={careersLoader} />
+        <Route path=":id" element={<CareersDetails />} loader={careerDetailsLoader}></Route>
       </Route>
 
       <Route path="*" element={<NotFound />} />
     </Route>
   )
-)
+);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/careers/CareersDetails.js
@@ -0,0 +1,26 @@
+import { useLoaderData, useParams } from "react-router-dom"
+
+export default function CareersDetails(){
+    const { id } = useParams()
+    const career = useLoaderData()
+    return (
+        <div className="careers-details">
+            <h2>Careers Details for {career.title}</h2>
+            <p>Starting salary: {career.salary}</p>
+            <p>Location: {career.location}</p>
+            <div className="details">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia veritatis mollitia ut consectetur, blanditiis, tempora facere labore dolores numquam, iste quis laboriosam dolorum obcaecati? Et cupiditate dicta earum dolore perferendis.</p>
+            </div>
+        </div>
+    )
+}
+
+
+export const careerDetailsLoader = async ({params}) => {
+    const { id } = params 
+
+    const res = await fetch("http://localhost:4000/careers/" + id)
+
+    return res.json()
+
+}
