e1c6a02ec14b182e8f4ac63f949ead91c1d46978
yomayo666 <yomayo1992@yandex.ru>
2023-11-09 16:28:48 +0300

    add: add carear page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
 import {
-  createBrowserRouter,
-  Routes,
-  Route,
-  Link,
-  NavLink,
+  createBrowserRouter, 
   createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
 
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Faq from './pages/help/Faq'
+import Contact from './pages/help/Contact'
+import NotFound from './pages/NotFound'
+import Careers, { careersLoader } from './pages/careers/Careers'
 
-import RootLayout from "./layouts/RootLayout";
-import HelpLayout from "./layouts/HelpLayout";
-import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
-import NotFound from "./pages/NotFound";
+// layouts
+import RootLayout from './layouts/RootLayout'
+import HelpLayout from './layouts/HelpLayout'
+import CareersLayout from './layouts/CareersLayout'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout/>}>
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route path="help" element={<HelpLayout/>}>
-        <Route path="faq" element={<Faq/>}></Route>
-        <Route path="contacts" element={<Contact/>}></Route>
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact/>} />
       </Route>
-      <Route path="*" element={<NotFound/>}/>
+      <Route path="careers" element={<CareersLayout />}>
+        <Route 
+          index 
+          element={<Careers />} 
+          loader={careersLoader} 
+        />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
-);
+)
+
 function App() {
   return (
-    <RouterProvider router={router}></RouterProvider>
-
+    <RouterProvider router={router} />
   );
 }
 
-export default App;
+export default App
